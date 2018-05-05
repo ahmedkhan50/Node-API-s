9fd465eb921a1054638949f1b7c3d57c54822287
Nabeel Ahmed Khan <nakhan@tracfone.com>
2018-05-04 23:12:16 -0400

    enviornments variable made dynamic

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('./config/config');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -6,7 +7,7 @@ const Todo = require('./models/todo').Todo;
 const User = require('./models/user').User;
 const ObjectId = require('mongodb').ObjectId;
 const _ = require('lodash');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const app = express();
 
 app.use(bodyParser.json());
